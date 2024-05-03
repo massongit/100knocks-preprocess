32aa56c7201beacf0179c52228cac8660432c95d
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-05-03 09:29:46 +0900

    Rインストール時のコア数を増やす

--- install.R
+++ install.R
@@ -2,5 +2,6 @@ install.packages(
   c("DBI", "RPostgreSQL", "themis"),
   dependencies = TRUE,
   error = TRUE,
-  repos = "https://cran.r-project.org"
+  repos = "https://cran.r-project.org",
+  Ncpus = max(parallel::detectCores() / 2, 1)
 )
